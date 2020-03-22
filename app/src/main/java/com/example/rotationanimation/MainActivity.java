84f2b9e542d484f6e1582b6a6bcfd5798443fec6
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-22 13:19:41 +0600

    coding Rotation

--- app/src/main/java/com/example/rotationanimation/MainActivity.java
+++ app/src/main/java/com/example/rotationanimation/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.rotationanimation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView arshadImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arshadImage = (ImageView) findViewById(R.id.imageViewArshad);
     }
 }
