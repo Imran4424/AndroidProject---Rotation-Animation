d98f41121927f472a98fa0535291f8434ad35b51
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-22 19:05:13 +0600

    coding Rotation

--- app/src/main/java/com/example/rotationanimation/MainActivity.java
+++ app/src/main/java/com/example/rotationanimation/MainActivity.java
@@ -3,21 +3,27 @@ package com.example.rotationanimation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView arshadImage;
-    Button
+    Button fullScreenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         arshadImage = (ImageView) findViewById(R.id.imageViewArshad);
+        fullScreenButton = (Button) findViewById(R.id.buttonFullScreen);
 
         // setting X position
         arshadImage.setX(-2000);
         arshadImage.animate().translationXBy(2000).rotation(360).setDuration(2000);
+    }
+
+    public void buttonPressed(View v) {
+        
     }
 }
